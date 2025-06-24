e477505f4d03b22407ad2ba763f9442db31999fc
joao <joaovitorcandidosilva@gmail.com>
2025-06-23 21:26:38 -0300

    primeira aula de js

---
+++ form.js
@@ -0,0 +1,7 @@
+btn_enviar.addEventListener('click', function(event){
+  let nome = document.getElementById('nome').value;
+  let idade = document.getElementById('idade').value;
+
+  console.log('Nome:', nome);
+  console.log('Idade:', idade);
+});
